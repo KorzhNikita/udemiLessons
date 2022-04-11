ba59da56ff445ed450dda6d9ea430cd95e9b00ee
Kit <korzh.nikita01@gmail.com>
2022-04-12 00:05:09 +0300

    first app with cycles

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let questionNumOfFilms = "Сколько фильмов Вы уже посмот
 let questionFilmName = "Один из последних просмотренных фильмов?";
 let questionFilmRating = "На сколько оцените его?";
 
-numberOfFilms = prompt(questionNumOfFilms, [0]);
+numberOfFilms = +prompt(questionNumOfFilms, [0]);
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -19,12 +19,23 @@ let personalMovieDB = {
     privat: false
 };
 
-let firstMovieName = prompt(questionFilmName, []),
-    firstMovieRating = prompt(questionFilmRating, []),
-    secondMovieName = prompt(questionFilmName, []),
-    secondMovieRating = prompt(questionFilmRating, []);
+//let firstMovieName = prompt(questionFilmName, []),
+//    firstMovieRating = prompt(questionFilmRating, []),
+//    secondMovieName = prompt(questionFilmName, []),
+//    secondMovieRating = prompt(questionFilmRating, []);
 
-personalMovieDB.movies[firstMovieName] = firstMovieRating;
-personalMovieDB.movies[secondMovieName] = secondMovieRating;
+for(let i = 0; i < 2; i++) {
+    personalMovieDB.movies[prompt(questionFilmName, [])] = prompt(questionFilmRating, []); 
+}
+
+if(personalMovieDB.count < 10) {
+    console.log('low');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('classic');
+} else if(personalMovieDB.count >= 30){
+    console.log('many');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDB);
