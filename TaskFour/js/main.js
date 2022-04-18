0428ed1f0549f370316229d95745a7a578a48205
Kit <korzh.nikita01@gmail.com>
2022-04-18 22:02:20 +0300

    Task Four

--- TaskFour/js/main.js
+++ TaskFour/js/main.js
@@ -1,108 +1,145 @@
-//К сожалению, я пока не до конца разобрался как работает генетический алгоритм, поэтому 4я задача решена 
-//более классическим способом.
-
-let json = {"corrections": [2, 8, 10, 7, 12], "cells": [8, 4, 6, 2, 2, 10, 6] };
+let json = {"corrections": [2, 8, 10, 7, 12], "cells": [8, 4, 6, 2, 2, 10, 10, 10, 6, 8] };
+//                          2  8  10  6  10 = 36     ///39
+//                                    2  4  = 3
 const corrections = json.corrections;
 const cells = json.cells;
 
+const GEN = 10000;
+const AGENT = 300;
 
-//Функция, которая возвращает наибольшее значение из массива
-function getMaxOfArray(numArray) {
-    return Math.max.apply(null, numArray);
-}
+let agents = [];
+let bestScore = 0;
+let bestAgents = [];
+let names = 0;
 
 
-//Подбираем наибольшие значения для главного двигателя
-function mainThruster (corrections, cells) {
-    let correctionsInterm = [];
+const count = (arr) => arr.reduce((acc, num) => acc + num, 0);
+const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+
+const getRandomInt = (max) => {return Math.floor(Math.random() * max);};
+
+let generateAgent = (corrections, cells) => {
+    let tempCells = JSON.parse(JSON.stringify(cells));
     let correctionsMain = [];
-    let iterations = corrections.length;
-    let cellsTemp = cells; 
-    for (let i = 0; i < iterations; i++) {
-        let maxCorrection = getMaxOfArray(corrections);
-        let maxCells = getMaxOfArray(cells);
-        
-        if (maxCorrection < maxCells) {
-            for (let i = 0; i < cellsTemp.length; i++) {
-                if (cellsTemp[i] > maxCorrection) {
-                    cellsTemp[i] = 0;
-                }
-            }
-            maxCells = getMaxOfArray(cellsTemp);
-            
+    let correctionsSec = [];
+    /*if (corrections.length % 2 == 0) {
+        for (let i = 0; i < corrections.length / 2; i++) {
+            let index1 = getRandomInt(tempCells.length);
+            let index2 = getRandomInt(tempCells.length);
+            correctionsMain[getRandomInt(corrections.length)] = tempCells[index1];
+            correctionsSec[getRandomInt(corrections.length)] = tempCells[index2];
+            tempCells.splice(index1, 1);
+            tempCells.splice(index2, 1);
         }
 
-        let indexOfMaxCorrection = corrections.indexOf(maxCorrection);
-        let indexOfMaxCells = cellsTemp.indexOf(maxCells);
-
-        if (maxCells <= maxCorrection && maxCells != 0) {
-            correctionsMain[indexOfMaxCorrection] = maxCells;
-            correctionsInterm[indexOfMaxCorrection] = corrections[indexOfMaxCorrection] - maxCells;
-            cells[indexOfMaxCells] = 0;
-            cellsTemp[indexOfMaxCells] = 0;
-            corrections[indexOfMaxCorrection] = 0;
-        }                
-    }
-
-    for (let i = 0; i < iterations; i++) {
-        if (correctionsInterm[i] == null) {
-            correctionsInterm[i] = corrections[i];
+    } else {*/
+        for (let i = 0; i < Math.floor(cells.length / 2) + 1; i++) {
+            let index1 = getRandomInt(tempCells.length);
+            correctionsMain[getRandomInt(corrections.length)] = tempCells[index1];
+            tempCells.splice(index1, 1);
         }
+        for (let i = 0; i < Math.floor(cells.length / 2); i++) {
+            let index2 = getRandomInt(tempCells.length);
+            correctionsSec[getRandomInt(corrections.length)] = tempCells[index2];
+            tempCells.splice(index2, 1);
+        }
+
+    //}
+
+
+    for (let i = 0; i < corrections.length; i++) {
         if (correctionsMain[i] == null) {
             correctionsMain[i] = 0;
         }
-    }
-
-    return {correctionsInterm: correctionsInterm, correctionsMain: correctionsMain, cells: cells};
-}
-
-//Подбираем наибольшие значения для второго двигателя
-function secThruster (correctionsInterm, cells) {
-    let correctionsSec = [];
-    let iterations = correctionsInterm.length;
-
-    for (let i = 0; i < iterations; i++) {
-        let maxCorrection = getMaxOfArray(correctionsInterm);
-        let maxCells = getMaxOfArray(cells);
-        let indexOfMaxCorrection = correctionsInterm.indexOf(maxCorrection);
-        let indexOfMaxCells = cells.indexOf(maxCells);
-
-        if (maxCells / 2 <= maxCorrection && maxCells != 0) {
-            correctionsSec[indexOfMaxCorrection] = maxCells;
-            correctionsInterm[indexOfMaxCorrection] = correctionsInterm[indexOfMaxCorrection] - maxCells;
-            cells[indexOfMaxCells] = 0;
-            correctionsInterm[indexOfMaxCorrection] = 0;
-        }   
-    }
-
-    for (let i = 0; i < iterations; i++) {
         if (correctionsSec[i] == null) {
             correctionsSec[i] = 0;
         }
     }
+    let delta_velocity = count(correctionsMain) + count(correctionsSec)/2;
+    nameAgent();
+    let agent = {main_thruster: correctionsMain, sec_thruster: correctionsSec, delta_velocity: delta_velocity, name: names};
+    //console.log(agent);
+    return agent;
+};
 
-    return {correctionsSec: correctionsSec};
+let addMutation = (agent) => {
+    let random = getRandomInt(100);
+    //console.log('random = ' + random);
+    let index1 = getRandomInt(agent.main_thruster.length);
+    let index2 = getRandomInt(agent.main_thruster.length);
+    let index3 = getRandomInt(agent.main_thruster.length);
+    let index4 = getRandomInt(agent.main_thruster.length);
+    
+    let temp = 0;
+    if (random < 50) {
+        temp = agent.main_thruster[index1];
+        agent.main_thruster[index1] = agent.main_thruster[index2];
+        agent.main_thruster[index2] = temp;
+    } else {
+        temp = agent.main_thruster[index1];
+        agent.main_thruster[index1] = agent.sec_thruster[index2];
+        agent.sec_thruster[index2] = temp;
+
+        temp = agent.main_thruster[index3];
+        agent.main_thruster[index3] = agent.sec_thruster[index4];
+        agent.sec_thruster[index4] = temp;
+
+    }
+
+    if (random > 50) {
+        shuffle(agent.main_thruster);
+        shuffle(agent.sec_thruster);
+    }
+    agent.delta_velocity = count(agent.main_thruster) + count(agent.sec_thruster)/2;
+
+    return JSON.parse(JSON.stringify(agent));
+};
+
+let nameAgent = () => {
+    names++;
+};
+
+let checkAgent = (agent, corrections) => {
+    for (let i = 0; i < corrections.length; i++) {
+        if (agent.main_thruster[i] + (agent.sec_thruster[i] / 2) > corrections[i]) {
+            return false;
+        }
+    } 
+    return true;
+};
+
+
+for(let i = 0; i < AGENT; i++) {
+    agents.push(generateAgent(corrections, cells));
 }
 
-//Подсчитываем сумму приращений скорости по всем маневрам
-const count = (arr) => arr.reduce((acc, num) => acc + num, 0);
 
 
-//Также можно заметить, что если в порядке корректировок большее значение будет идти после меньшего
-//а максимально доступная капсула будет уже использована для меньшего значения, то прирост скорости
-//на самой значимой корректировке будет недостаточным, однако на суммарное значение это не влияет, так как
-// в любом случае все наиболее производительные капсулы будут использованы именно в первом двигателе без
-//потери производительности. 
-function satelliteAlgorithm (corrections, cells) {
-    let main = mainThruster(corrections, cells);
-    let sec = secThruster(main.correctionsInterm, cells);
-    let delta_velocity = count(main.correctionsMain) + count(sec.correctionsSec)/2;
-    return JSON.stringify({main_thruster: main.correctionsMain, sec_thruster: sec.correctionsSec, delta_velocity: delta_velocity});
+for (let i = 0; i < GEN; i++) {
+    
+    //console.log('GEN - ' + i);
+    agents.forEach( (el) => {
+        /*console.log('______________________')
+        console.log('agent #' + agents.indexOf(el) + ', agent name - ' + el.name + ' ' + checkAgent(el, corrections) + ' delta_velocity = ' + el.delta_velocity);
+        console.log(el.main_thruster);
+        console.log(el.sec_thruster);
+        console.log('______________________')*/
+        if (/*checkAgent(el, corrections) && */el.delta_velocity > bestScore / 2) {
+            if ((el.delta_velocity > bestScore || el.delta_velocity == bestScore) && checkAgent(el, corrections)) {
+                bestScore = el.delta_velocity;
+                bestAgents.push(JSON.parse(JSON.stringify(el)));
+            }
+            el = JSON.parse(JSON.stringify(addMutation(el)));
+        } else {
+            agents.splice(agents.indexOf(el), 1);
+            agents.push(generateAgent(corrections, cells));
+        }
+    });
 }
 
-console.log(json);
-console.log(satelliteAlgorithm(corrections, cells));
-
-
+console.log('________________');
+console.log(bestScore);
+console.log(bestAgents);
+console.log(corrections);
 
 
